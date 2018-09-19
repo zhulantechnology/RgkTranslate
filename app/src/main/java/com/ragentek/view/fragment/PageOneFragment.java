07f8c330ae419341f900edb341d12040491aaec1
Jeffreywangjun <wangjunahasd@163.com>
2018-09-19 17:19:25 +0800

    实现了自定义的spinner
    Signed-off-by: Jeffreywangjun <wangjunahasd@163.com>

--- app/src/main/java/com/ragentek/view/fragment/PageOneFragment.java
+++ app/src/main/java/com/ragentek/view/fragment/PageOneFragment.java
@@ -1,5 +1,7 @@
 package com.ragentek.view.fragment;
 
+import android.content.Context;
+import android.content.res.TypedArray;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
@@ -12,6 +14,8 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -22,6 +26,7 @@ import com.ragentek.database.SPManager;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -36,6 +41,7 @@ public class PageOneFragment extends Fragment {
     private TextView dateView;
     private Spinner countrySpinner;
     private updateBg listerner;
+    private Context mContext;
 
 
     @Nullable
@@ -43,15 +49,24 @@ public class PageOneFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater,
                              @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_pageone_layout, container, false);
+        mContext = getActivity();
+        initSpinnerData();
+     
+        SimpleAdapter simpleAdapter = new SimpleAdapter(
+                mContext, initSpnnerData(), R.layout.spinner_item_layout,
+                new String[] { "img", "text" }, new int[] {
+                R.id.image_view, R.id.text_view });
 
         timeView = view.findViewById(R.id.time_view);
         dateView = view.findViewById(R.id.date_view);
 
         countrySpinner = view.findViewById(R.id.country_spinner_view);
         // 声明一个ArrayAdapter用于存放简单数据
-        ArrayAdapter<String> spinnerAdapter = new ArrayAdapter<String>(
-                getActivity(), android.R.layout.simple_spinner_item, getSpinnerData());
-        countrySpinner.setAdapter(spinnerAdapter);
+       /* ArrayAdapter<String> spinnerAdapter = new ArrayAdapter<String>(
+                mContext, android.R.layout.simple_spinner_item, getSpinnerData());
+        countrySpinner.setAdapter(spinnerAdapter);*/
+
+        countrySpinner.setAdapter(simpleAdapter);
 
         int currentCountry = SPManager.getInstance().getInt(SPManager.CURRENT_COUNTRY, 0);
         countrySpinner.setSelection(currentCountry);
@@ -61,7 +76,7 @@ public class PageOneFragment extends Fragment {
             public void onItemSelected(AdapterView<?> parent, View view,
                                        int position, long id) {
                 // 在选中之后触发
-                Toast.makeText(getActivity(),
+                Toast.makeText(mContext,
                         parent.getItemAtPosition(position).toString(),
                         Toast.LENGTH_SHORT).show();
                 //listerner.changeWallpaper(position);   // don't change
@@ -90,6 +105,41 @@ public class PageOneFragment extends Fragment {
         dataList.add("英国");
         dataList.add("俄罗斯");
         return dataList;
+    }
+
+
+    public List<Map<String, Object>> initSpnnerData() {
+        //生成数据源
+        List<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
+        //每个Map结构为一条数据，key与Adapter中定义的String数组中定义的一一对应。
+        for (int i = 0;i < countryStrArray.size();i++) {
+            Map<String, Object> map = new HashMap<String, Object>();
+            map.put("img", countryIconArray.getResourceId(i, 0));
+            map.put("text", countryStrArray.get(i));
+            list.add(map);
+        }
+        
+        return list;
+    }
+
+    private List<String> countryStrArray;
+    private TypedArray countryIconArray;
+
+    private void initSpinnerData() {
+        final String[] codeArray = getContext().getResources()
+                .getStringArray(R.array.country_string_array);
+        countryStrArray = new ArrayList<>();
+        for (String str : codeArray) {
+            countryStrArray.add(str);
+        }
+        countryIconArray = mContext.getResources().obtainTypedArray(R.array.country_icons_array);
+    /*    TypedArray profileArray = mContext.getResources().obtainTypedArray(R.array.language_profile_picture_array);
+        if (position >= 0 && position < profileArray.length()) {
+            TextView tv = (TextView) v;
+            int id = profileArray.getResourceId(position, 0);
+            tv.setCompoundDrawablesWithIntrinsicBounds(mContext.getDrawable(id), null, null, null);
+            tv.setCompoundDrawablePadding(80);
+        }*/
     }
 
     private void getDate() {
